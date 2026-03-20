5dc65b20ec8f4e8af678d2febf7cd503133168cb
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2026-03-20 13:27:37 -0400

    indents

--- web/packages/extension/src/background.ts
+++ web/packages/extension/src/background.ts
@@ -253,10 +253,9 @@ function onMessage(
 ): void {
     if (isMessage(request)) {
         if (request.type === "open_url_in_player") {
-            const index =
-                sender.tab?.index !== undefined
-                    ? sender.tab.index + 1
-                    : undefined;
+            const index = sender.tab?.index !== undefined
+                ? sender.tab.index + 1
+                : undefined;
             chrome.tabs.create({
                 url: utils.runtime.getURL(`player.html#${request.url}`),
                 index,
