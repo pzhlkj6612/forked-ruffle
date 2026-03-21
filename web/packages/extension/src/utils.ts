3d9986f4e8edc1a85420d398bc8491af81f422ac
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2026-03-20 23:01:28 -0400

    restore the code style

--- web/packages/extension/src/utils.ts
+++ web/packages/extension/src/utils.ts
@@ -66,11 +66,11 @@ async function getAdjacentTabIndex(): Promise<number | undefined> {
     });
     return activeTab?.index !== undefined ? activeTab.index + 1 : undefined;
 }
-export async function openPlayerPage(): Promise<void> {
+export const openPlayerPage: () => Promise<void> = async () => {
     const index = await getAdjacentTabIndex();
     await tabs.create({ url: "/player.html", index });
 };
-export async function openOnboardPage(): Promise<void> {
+export const openOnboardPage: () => Promise<void> = async () => {
     const index = await getAdjacentTabIndex();
     await tabs.create({ url: "/onboard.html", index });
 };
